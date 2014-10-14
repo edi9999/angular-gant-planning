b198c27f3a6287bf8cf89d7c4d0b85092aa57d09
Edgar Hipp <hipp.edg@gmail.com>
2014-10-14 14:21:29 +0200

    angular-gant-planning first version

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-angular.module('gant-planning')
-    .directive('gant-planning',function () {
-
-    });
+angular.module('gantPlanning',[])
+.directive('gantPlanning',function () {
+    return{
+        link:link
+    };
+    function link(scope, element, attrs) {
+        var planning=gantplanning
+            .createPlanning()
+            .fill([
+                {"description":"First Phase","start":0,"end":2},
+                {"description":"Second Phase","start":3,"end":6}
+            ])
+            .attachTo(element[0])
+            .draw();
+    }
+});
